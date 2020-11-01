615060e48475605e254cdf8eaf3ed836bca7b7b0
Ugo Emekauwa <51210375+ugo-emekauwa@users.noreply.github.com>
2020-10-31 23:58:34 -0400

    Add files via upload

--- block_hx_hyperv_ssh.ps1
+++ block_hx_hyperv_ssh.ps1
@@ -8,11 +8,22 @@ $HxClusterIP = 192.168.0.100
 # Set HyperFlex Service Controller VM IP
 $ServiceControllerVmIp = 192.168.0.104
 
-# Block SSH Access to HyperFlex Cluster IP
+# Block SSH Access on to HyperFlex Cluster IP
 Add-VMNetworkAdapterExtendedAcl -VMName "StCtlVM" -LocalIPAddress $HxClusterIP -Action "Deny" -Direction "Inbound" -LocalPort 22 -Protocol "TCP" -Weight 10
 
 # Block SSH Access to HyperFlex Service Controller VM IP
 Add-VMNetworkAdapterExtendedAcl -VMName "StCtlVM" -LocalIPAddress $ServiceControllerVmIp -Action "Deny" -Direction "Inbound" -LocalPort 22 -Protocol "TCP" -Weight 11
+
+# Allow SSH Access on to HyperFlex Cluster IP
+Add-VMNetworkAdapterExtendedAcl -VMName "StCtlVM" -LocalIPAddress 192.168.0.100 -Action "Allow" -Direction "Inbound" -RemoteIPAddress 192.168.0.100/32 -LocalPort 22 -Protocol "TCP" -Weight 12
+Add-VMNetworkAdapterExtendedAcl -VMName "StCtlVM" -LocalIPAddress 192.168.0.100 -Action "Allow" -Direction "Inbound" -RemoteIPAddress 192.168.0.170/32 -LocalPort 22 -Protocol "TCP" -Weight 13
+Add-VMNetworkAdapterExtendedAcl -VMName "StCtlVM" -LocalIPAddress 192.168.0.100 -Action "Allow" -Direction "Inbound" -RemoteIPAddress 192.168.0.200/32 -LocalPort 22 -Protocol "TCP" -Weight 14
+
+# Allow SSH Access to HyperFlex Service Controller VM IP
+Add-VMNetworkAdapterExtendedAcl -VMName "StCtlVM" -LocalIPAddress 192.168.0.104 -Action "Allow" -Direction "Inbound" -RemoteIPAddress 192.168.0.100/32 -LocalPort 22 -Protocol "TCP" -Weight 15
+Add-VMNetworkAdapterExtendedAcl -VMName "StCtlVM" -LocalIPAddress 192.168.0.104 -Action "Allow" -Direction "Inbound" -RemoteIPAddress 192.168.0.170/32 -LocalPort 22 -Protocol "TCP" -Weight 16
+Add-VMNetworkAdapterExtendedAcl -VMName "StCtlVM" -LocalIPAddress 192.168.0.104 -Action "Allow" -Direction "Inbound" -RemoteIPAddress 192.168.0.200/32 -LocalPort 22 -Protocol "TCP" -Weight 17
+
 
 # Exit
 Exit
